878e341ee5574bd7101ff445b0a73622e2e31d3e
Ahmed Znagui <amed.bob88@gmail.com>
2026-03-16 23:02:05 +0100

    first commit

---
+++ consegna1-loader.cpp
@@ -0,0 +1,170 @@
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+using namespace std;
+
+//* compilazione: g++ consegna1-loader.c
+//
+
+// Il programma carica il file data.csv contenente 100 righe con dati da ordinare in modo crescente
+// ./a.out
+// In output viene mostrato il numero di accessi in read alla memoria per eseguire il sorting di ciascuna riga
+
+// Obiettivo:
+// Creare un algoritmo di sorting che minimizzi la somma del numero di accessi per ogni sorting di ciascuna riga del file
+
+int ct_swap = 0;
+int ct_cmp = 0;
+int ct_read = 0;
+
+int max_dim = 0;
+int ntests = 100;
+int ndiv = 1;
+int details = 0;
+int graph = 0;
+
+int n = 0; /// dimensione dell'array
+
+void print_array(int *A, int dim) {
+    for (int j = 0; j < dim; j++) {
+        printf("%d ", A[j]);
+    }
+    printf("\n");
+}
+
+void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+    /// aggiorno contatore globale di swap
+    ct_swap++;
+}
+
+void bitonicMerge(int *A, int low, int count, int direction){
+    if(count > 1){
+        int k = count / 2;
+
+        //* confronto e scambio gli elementi dalle due metà
+        for(int i = low; i < low + k; i++){
+            ct_read += 2;
+            ct_cmp++;
+            //* determinare se due elementi di a sono fuori ordine rispetto alla direzione di ordinamento.
+            if((direction == 1 && A[i] > A[i + k]) || (direction == 0 && A[i] < A[i + k])){
+                ct_read += 2;
+                swap(A[i], A[i + k]);
+            }
+        }
+
+        bitonicMerge(A, low, k, 1);
+        bitonicMerge(A, low + k, k, direction);
+
+    }
+}
+
+void bitonicSort(int *A, int low, int count, int direction){
+    if(count > 1){
+        int k = count / 2;
+
+        bitonicSort(A, low, k, 1);
+        bitonicSort(A, low + k, k, 0);
+        bitonicSort(A, low, count, direction);
+    }
+
+    
+}
+
+int parse_cmd(int argc, char **argv) {
+
+    /// parsing argomento
+    max_dim = 1000;
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][1] == 'd')
+            ndiv = atoi(argv[i] + 3);
+        if (argv[i][1] == 't')
+            ntests = atoi(argv[i] + 3);
+        if (argv[i][1] == 'v')
+            details = 1;
+        if (argv[i][1] == 'g') {
+            graph = 1;
+            ndiv = 1;
+            ntests = 1;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int i, test;
+    int *A;
+    int *B; /// buffer per visualizzazione algoritmo
+
+    if (parse_cmd(argc, argv))
+        return 1;
+
+    /// allocazione array
+    A = new int[1024];
+
+    n = max_dim;
+
+    ifstream input_data;
+    input_data.open("data.csv");
+
+    int read_min = -1;
+    int read_max = -1;
+    long read_avg = 0;
+
+    //// lancio n tests volte per coprire diversi casi di input random
+    for (test = 0; test < ntests; test++) {
+
+        /// inizializzazione array: numeri random con range dimensione array
+        for (i = 0; i < n; i++) {
+            char comma;
+            input_data >> A[i];
+            input_data >> comma;
+        }
+
+        if (details) {
+            printf("caricato array di dimensione %d\n", n);
+            print_array(A, n);
+        }
+
+        ct_swap = 0;
+        ct_cmp = 0;
+        ct_read = 0;
+
+        int nPadded = 1024;
+        for(int i = 1000; i < nPadded - 1; i++){
+            A[i] = 2147483647;
+        }
+
+        /// algoritmo di sorting
+        bitonicSort(A, 0, nPadded, 1);
+
+        if (details) {
+            printf("Output:\n");
+            print_array(A, n);
+        }
+
+        /// statistiche
+        read_avg += ct_read;
+        if (read_min < 0 || read_min > ct_read)
+            read_min = ct_read;
+        if (read_max < 0 || read_max < ct_read)
+            read_max = ct_read;
+        printf("Test %d %d\n", test, ct_read);
+    }
+
+    printf("N test: %d, Min: %d, Med: %.1f, Max: %d\n",
+           ntests,
+           read_min, (0.0 + read_avg) / ntests, read_max);
+
+    delete[] A;
+
+    return 0;
+}
